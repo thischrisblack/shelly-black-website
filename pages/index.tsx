8bd195168e57558edf9fb9e70bf41de18f29976d
Chris Black <black@chrisblack.net>
2020-12-29 17:30:50 -0500

    Started on home page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import styles from './Home.module.scss';
 import Layout from '../components/Layout';
+import { useEffect, useRef, useState } from 'react';
 
 const Index = ({
     title,
@@ -9,9 +10,60 @@ const Index = ({
     title: string;
     description: string;
 }) => {
+    const [isShown, setShown] = useState(false);
+
+    const moreDiv = useRef(null);
+
+    // Show full about text.
+    useEffect(() => {
+        moreDiv.current.style.opacity = `${isShown ? '1' : '0'}`;
+    });
+
     return (
         <Layout pageTitle={title}>
-            <article className={styles.post}>WOAH</article>
+            <article className={styles.content}>
+                <div className={styles.left}>
+                    <p>
+                        Hello! I'm Shelly, a Japanese Mexican American librarian
+                        and photographer in Raleigh, NC.{' '}
+                        <a onClick={() => setShown(!isShown)}>
+                            {isShown ? '' : '[ more ]'}
+                        </a>
+                    </p>
+                    <div className={styles.more} ref={moreDiv}>
+                        <p>
+                            I'm currently the Cyma Rubin Library Fellow at North
+                            Carolina State University Libraries where I support
+                            digital preservation in Special Collections.
+                            Previously I was a marketing specialist at the
+                            University of Arizona Libraries and was selected as
+                            a 2020 Emerging Leader by the American Library
+                            Association. During my student days, I worked in
+                            special collections and fine art photography
+                            archives.
+                        </p>
+                        <p>
+                            As a photographer, I've exhibited in spaces across
+                            the US and abroad, such as: Center for Creative
+                            Photography, Tucson, AZ; University of Arizona
+                            Museum of Art; Tucson International Airport; West
+                            Valley Art Museum, Surprise, AZ; ISE Cultural
+                            Foundation, New York, NY; and the International
+                            Photography Festival, Pingyao, China. My photography
+                            has also appeared on wine labels and in alt-weekly
+                            music columns. I've also been a professional jewelry
+                            photographer and volunteer photography instructor
+                            for refugees.
+                        </p>
+                        <p>
+                            I also have 10 years of experience in marketing and
+                            skills including copywriting, graphic design, and
+                            social media.
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.right}></div>
+            </article>
         </Layout>
     );
 };
